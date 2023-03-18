5ce8da367312ba4af30a0acd64de4ef6b52eaeb2
IkramBackend <ikramxiyasov2@gmail.com>
2023-03-18 16:33:30 +0500

    inital commit

--- main.py
+++ main.py
@@ -10,12 +10,27 @@ array = [{
     id: 3,
     'age': 19,
     'name': "JoK"
+}, {
+    id: 4,
+    'age': 29,
+    'name': "oLoa"
+}, {
+    id: 5,
+    'age': 19,
+    'name': "KOK"
+}, {
+    id: 6,
+    'age': 69,
+    'name': "JlK"
 }]
+maximum = int(len(array))
+minimum = 1
 
-def getInfo(id_Number):
-    for item in array:
-        if item[id] == id_Number:
-            return f"ID: {item[id]}\nName: {item['name']}\nAge {item['age']}"
+def getInfo(x = minimum, y=maximum):
+    result = array[x:y]
+    storage = ""
+    for item in result:
+         storage += f"{str(item)}\n"
+    return storage
 
-
-print(getInfo(1))
+print(getInfo(1, 5))
